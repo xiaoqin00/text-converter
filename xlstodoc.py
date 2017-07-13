0854acb74417fdf5a037da799250b670e891bda9
00 <honer00@163.com>
2017-07-13 10:41:27 +0800

    add

--- xlstodoc.py
+++ xlstodoc.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Created by xiaoqin00 on 2017/6/26
-
+# Created by xiaoqin00 on 2017/7/10
+import xlrd
 from optparse import OptionParser
-from docx import Document
-from docx.shared import Inches
+
 def convert():
-    #创建 Document 对象，相当于打开一个 word 文档
+    try:
+        data = xlrd.open_workbook(input)
+        table = data.sheets()[0]
+        nrows = table.nrows
+        ncols=table.ncols
+        # print nrows, type(nrows)
+        f = open('tmp.txt', 'w')   #临时txt文件
+        for i in range(nrows):
+            tmp=''
+            for j in range(ncols):
+                print table.cell(i,j)
+                tmp = tmp + str(table.cell(i, j)).split(':')[1] +' '
+            f.write(tmp + '\n')
+        f.close()
+    except Exception,e:
+        print e
+    from docx import Document
+    from docx.shared import Inches
+
+    # 创建 Document 对象，相当于打开一个 word 文档
     document = Document()
 
     # #向文档中添加一个标题，标题级别设置为0级
     # document.add_heading('This is title', level=0)
 
-    #向文档中添加一个段落，并将段落引用赋给变量 p
-    #使用 add_run 方法追加字段，并设置格式
+    # 向文档中添加一个段落，并将段落引用赋给变量 p
+    # 使用 add_run 方法追加字段，并设置格式
     # f=open('test.pdf.txt','r')
-    f=open(str(options.input),'r')
+    f = open('tmp.txt', 'r')
     for i in f.readlines():
-        print i
-        print 'test'
-        i=str(i)
-        i=i.split()
-        if not i:
-            i='\t'
-            # continue
-        p = document.add_paragraph(i)
+     print i
+     print 'test'
+     i = str(i)
+     i = i.split()
+     if not i:
+      i = '\t'
+      # continue
+     p = document.add_paragraph(i)
 
     # p.add_run('bold').bold = True
     # p.add_run(' and some ')
@@ -46,9 +64,9 @@ def convert():
     # table.cell(1,0).text = "cell_10"
     # table.cell(1,1).text = "cell_11"
 
-    #保存文本
+    # 保存文本
     if options.output:
-        document.save(options.output)
+     document.save(options.output)
     document.save(str(output))
     return
 
